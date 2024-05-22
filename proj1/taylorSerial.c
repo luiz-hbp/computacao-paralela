649d1547cd13b0e5388bfec1a00d3d8eeec04bdc
Luiz HB Pasquinelli <144058147+luiz-hbp@users.noreply.github.com>
2024-05-22 08:00:42 -0300

    Comentarios Código Serial

--- proj1/taylorSerial.c
+++ proj1/taylorSerial.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-long double calculoE(int n){
-        long double eEstimado = 2.0;
+long double calculoE(int n){ //Funcão calculoE recebe número de séries e retorna o número de Euler estimado.
+        long double eEstimado = 2.0; //ínicia valor estimado como 2 e já poupa o calculo das duas primeiras séries
         long double fat = 1.0;
-        for (int i = 2; i < n; i++){
-                fat = fat*i;
-                eEstimado += 1.0/fat;
+        for (int i = 2; i < n; i++){//Partindo da terceira série parte por todos os números de séries
+                fat = fat*i;//Calcula o falor do fatorial baseando-se no valor anterior
+                eEstimado += 1.0/fat;//Incrementa o valor estimado com o valor da série atual
                 //printf("%d %0.70Lf\n", i, eEstimado);
         }
         return eEstimado;
